71ddc189fcba8a85e993feb76b103aa125cf3ef4
Pavel-droid <72227704+Pavel-droid@users.noreply.github.com>
2020-11-09 23:25:42 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 ## First function, makeCacheMatrix() creates an R object that stores a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
